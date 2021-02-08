369188974c92c04a774dcc2bcd62c796b88bbf0a
Pabcargam <pabcargam@alum.us.es>
2021-02-08 19:35:08 +0100

    Comentados los test de Visualizer

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+# from django.test import TestCase
+# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.keys import Keys
 
 # from base.tests import BaseTestCase
 
